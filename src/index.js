fb139d14521a26456acde1018550effa8404332d
Wojciech <wojciechonoszko@gmail.com>
2022-04-20 13:03:19 +0200

    Update package-lock.json, package.json, and 2 more files...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-import './sass/main.scss';
+// npm install chart.js --save - chart.js library installation
+
+import Chart from 'chart.js'
+
+const container = document.getElementById('myChart');
+const btn = document.getElementById('btn');
+
+const prepareChart = () => {
+    const DataSet = (size, value, label) => {
+        const generateValues = () =>
+        [...Array(size)]
+            .fill(value)
+            .map(v => v *Math.random());
+
+        const generateNumber = (min, max) =>
+            Math.random() * (max - min) + min;
+        const generateRGB = () =>
+            [...Array(size)]
+                .map(v => [generateNumber(1, 255), generateNumber(1, 255), generateNumber(1, 255)]);
+
+        const generateLabels = () =>
+            [...Array(size).keys()]
+                .map(v => `${label} NO. ${v + 1}`);
+
+        const generateColorsAndBorders = () => {
+            const arr = generateRGB();
+            return [
+                arr.map(v => `rgba(${v[0]}, ${v[1]}, ${v[2]}, 0.2)`),
+                arr.map(v => `rgba(${v[0]}, ${v[1]}, ${v[2]}, 1)`)
+            ]
+        };
+        const colorAndBorders = generateColorsAndBorders();
+
+        return {
+            labels: generateLabels(),
+            values: generateValues(),
+            colors: colorAndBorders[0],
+            borders: colorAndBorders[1]
+        };
+    };
+    const SIZE = 6;
+    const VALUE = 10;
+    const LABEL = 'SALARY';
+    
+}
+
